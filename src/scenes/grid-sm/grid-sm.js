f48ae1abe5ac1d6516f86df4e43c161ca270678d
Jaime Gonzalez <jaimegonzalez@Jaimes-MacBook-Pro.local>
2019-10-18 15:00:37 -0700

    Added Nike

--- src/scenes/grid-sm/grid-sm.js
+++ src/scenes/grid-sm/grid-sm.js
@@ -1,31 +1,34 @@
-import React, { Component } from 'react';
-import {Grid,Image} from 'semantic-ui-react';
-import "./grid-sm.css";
+import React, { Component } from 'react'
+import { Grid, Image } from 'semantic-ui-react'
+import './grid-sm.css'
 
 class grid extends Component {
-    
-
-
-    render () {
-        return (
-        <div className="grid">
-            <Grid >
-                <Grid.Row>
-                <Image className='icon' src={this.props.github} avatar />
-                <a href="https://github.com/jaimejgonzalez1" rel="noopener noreferrer" target="_blank" className='shift'>www.github.com/jaimejgonzalez1</a>
-                </Grid.Row>
-                <Grid.Row>
-                <Image className='icon' src={this.props.link} avatar />
-                <a href="https://www.linkedin.com/in/jaimejgonzalez-1/" rel="noopener noreferrer" target="_blank" className='shift'>www.linkedin.com/in/jaimejgonzalez-1</a>
-                </Grid.Row>
-                <Grid.Row>
-                <Image className='icon'  src={this.props.mail}  avatar />
-                <a href="mailto:hello@jaimejgonzalez.com?Subject=Hello!"  className='shift'>hello@jaimejgonzalez.com</a>
-                </Grid.Row>
-            </Grid>
-          </div>
-        );
-    }
+  render() {
+    return (
+      <div className='grid'>
+        <Grid>
+          <Grid.Row>
+            <Image className='icon' src={this.props.github} avatar />
+            <a href='https://github.com/jaimejgonzalez1' rel='noopener noreferrer' target='_blank' className='shift'>
+              www.github.com/jaimejgonzalez1
+            </a>
+          </Grid.Row>
+          <Grid.Row>
+            <Image className='icon' src={this.props.link} avatar />
+            <a href='https://www.linkedin.com/in/jaimejgonzalez-1/' rel='noopener noreferrer' target='_blank' className='shift'>
+              www.linkedin.com/in/jaimejgonzalez-1
+            </a>
+          </Grid.Row>
+          <Grid.Row>
+            <Image className='icon' src={this.props.mail} avatar />
+            <a href='mailto:jaimejgonzalez1@gmail.com?Subject=Hello!' className='shift'>
+              jaimejgonzalez1@gmail.com
+            </a>
+          </Grid.Row>
+        </Grid>
+      </div>
+    )
+  }
 }
 
-export default grid;
+export default grid
--- src/scenes/navbar/navbar.js
+++ src/scenes/navbar/navbar.js
@@ -1,156 +1,152 @@
-import React, { Component } from 'react';
-import './navbar.css';
-import {
-  Container,
-  Menu,
-  Responsive,
-  Segment,
-  Visibility,
-  Image
-} from 'semantic-ui-react'
-import Hero from '../../components/hero/hero';
-import Render from '../render/render';
-import SmallGrid from '../grid-sm/grid-sm';
+import React, { Component } from 'react'
+import './navbar.css'
+import { Container, Menu, Responsive, Segment, Visibility, Image } from 'semantic-ui-react'
+import Hero from '../../components/hero/hero'
+import Render from '../render/render'
+import SmallGrid from '../grid-sm/grid-sm'
 import Github from '../../assets/media/images/github-2.png'
 import LinkedIn from '../../assets/media/images/linkedin.png'
 import Mail from '../../assets/media/images/mail.png'
 import UP from '../../assets/media/images/up-arrow.png'
 import RightCard from '../../components/rightCard/rightCard'
-import Work from '../works/works';  
+import Work from '../works/works'
 import Profile from '../../assets/media/images/cubism.png'
 class navbar extends Component {
-
   state = { activeItem: 'JAIME GONZALEZ' }
   handleItemClick = (e, { name }) => this.setState({ activeItem: name })
   render() {
     const { activeItem } = this.state
     return (
       <div>
-      <div id="backDrop">
-      <Render/>
-      </div>
-      <div id="landPage">
-      <Responsive {...Responsive.onlyComputer}>
-        <Visibility>
-          <Segment  textAlign='center' style={{ minHeight: 700, padding: '4em 0em' }} vertical>
-          {activeItem === 'JAIME GONZALEZ' ? (
-            <div>
-            <Hero small="Jaime Gonzalez | Full Stack Developer"/>
-            <Hero big="Hello."para="Come check out my work!"/>
-            </div>
-          ) : activeItem === 'ABOUT' ? (
-            <div>
-            <Hero big='ABOUT.'/>
-            <Image src={Profile} size='small' circular />
-            <RightCard para="I am a full-stack Software Developer with a focus on User Interfaces and User Experience.
-            My most recent work has been built in Angular, NodeJS, and MongoDB, however I started my career in 
-            .NET and Sql Server. Javascript is definitely my favorite. 
-            "/>
-            </div>
-          ): activeItem === 'CONTACT' ? (
-            <div>
-            <Hero big='CONTACT.' small="You can reach me anywhere"/>
-            <SmallGrid github= {Github} link ={LinkedIn} res={UP} mail={Mail}  />
-            </div>
-          ): activeItem === 'WORKS' ? (
-            <Work big="WORKS."/>
-          ): null}
-     
+        <div id='backDrop'>
+          <Render />
+        </div>
+        <div id='landPage'>
+          <Responsive {...Responsive.onlyComputer}>
+            <Visibility>
+              <Segment textAlign='center' style={{ minHeight: 700, padding: '4em 0em' }} vertical>
+                {activeItem === 'JAIME GONZALEZ' ? (
+                  <div>
+                    <Hero small='Jaime Gonzalez | Full Stack Developer' />
+                    <Hero big='Hello.' />
+                    <RightCard para='Come check out my work!' />
+                  </div>
+                ) : activeItem === 'ABOUT' ? (
+                  <div>
+                    <Hero big='ABOUT.' />
+                    <RightCard
+                      para='I am a full-stack Software Developer with a focus on User Interfaces and User Experience.
+            My most recent work has been built in VueJS, Angular, NodeJS, and MongoDB, however I started my career in 
+            .NET and Sql. Javascript is definitely my favorite though. 
+            '
+                    />
+                  </div>
+                ) : activeItem === 'CONTACT' ? (
+                  <div>
+                    <Hero big='CONTACT.' small='You can reach me anywhere' />
+                    <SmallGrid github={Github} link={LinkedIn} res={UP} mail={Mail} />
+                  </div>
+                ) : activeItem === 'WORKS' ? (
+                  <Work big='WORKS.' />
+                ) : null}
 
-            <Container>
-              <Menu id='navbar' pointing secondary fixed='bottom' size='massive'>
-              <Menu.Item name='WORKS' active={activeItem === 'WORKS'} onClick={this.handleItemClick} />
-              <Menu.Item name='ABOUT' active={activeItem === 'ABOUT'} onClick={this.handleItemClick} />
-              <Menu.Item name='CONTACT' active={activeItem === 'CONTACT'} onClick={this.handleItemClick} />
-              <Menu.Menu position='right'>
-                <Menu.Item name='JAIME GONZALEZ' active={activeItem === 'JAIME GONZALEZ'} onClick={this.handleItemClick} />
-              </Menu.Menu>
-            </Menu>
-          </Container>
-          </Segment>
-        </Visibility>
-      </Responsive>
-      <Responsive {...Responsive.onlyMobile}>
-        <Visibility>
-        <Segment textAlign='center' style={{ minHeight: 700, padding: '7em 0em' }} vertical>
-        {activeItem === 'JAIME GONZALEZ' ? (
-            <div>
-            <Hero small="Jaime Gonzalez | Full Stack Developer"/>
-            <Hero big="Hello."para="Come check out my work!"/>
-            </div>
-          ) : activeItem === 'ABOUT' ? (
-            <div>
-            <Hero big='ABOUT.'/>
-          
-            <RightCard para="I am a full-stack Software Developer with a focus on User Interfaces and User Experience.
-            My most recent work has been built in Angular, NodeJS, and MongoDB, however I started my career in 
-            .NET and Sql Server. Javascript is definitely my favorite. 
-            "/>
-            </div>
-          ): activeItem === 'CONTACT' ? (
-            <div>
-            <Hero big='CONTACT.' small="You can reach me anywhere"/>
-            <SmallGrid github= {Github} link ={LinkedIn} res={UP} mail={Mail}  />
-            </div>
-          ): activeItem === 'WORKS' ? (
-            <Work big="WORKS."/>
-          ): null}
-              <Container>
-                <Menu id='navbar' fixed="top"pointing secondary size='large'>
-                <Menu.Item name='WORKS' active={activeItem === 'Experience'} onClick={this.handleItemClick} />
-                <Menu.Item name='ABOUT' active={activeItem === 'ABOUT'} onClick={this.handleItemClick} />
-                <Menu.Item name='CONTACT' active={activeItem === 'CONTACT'} onClick={this.handleItemClick} />
-                <Menu.Menu position='right'>
-                  <Menu.Item name='JG' active={activeItem === 'JAIME GONZALEZ'} onClick={this.handleItemClick} />
-                </Menu.Menu>
-              </Menu>
-            </Container>
-            </Segment>
-          </Visibility>
-      </Responsive>
-      <Responsive {...Responsive.onlyTablet}>
-      <Visibility>
-        <Segment textAlign='center' style={{ minHeight: 700, padding: '7em 0em' }} vertical>
-        {activeItem === 'JAIME GONZALEZ' ? (
-            <div>
-            <Hero small="Jaime Gonzalez | Full Stack Developer"/>
-            <Hero big="Hello."para="Come check out my work!"/>
-            </div>
-          ) : activeItem === 'ABOUT' ? (
-            <div>
-            <Hero big='ABOUT.'/>
-          
-            <RightCard para="I am a full-stack Software Developer with a focus on User Interfaces and User Experience.
-            My most recent work has been built in Angular, NodeJS, and MongoDB, however I started my career in 
-            .NET and Sql Server. Javascript is definitely my favorite. 
-            "/>
-            </div>
-          ): activeItem === 'CONTACT' ? (
-            <div>
-            <Hero big='CONTACT.' small="You can reach me anywhere"/>
-            <SmallGrid github= {Github} link ={LinkedIn} res={UP} mail={Mail}  />
-            </div>
-          ): activeItem === 'WORKS' ? (
-            <Work big="WORKS."/>
-          ): null}
-              <Container>
-                <Menu id='navbar'  fixed="top"pointing secondary size='large'>
-                <Menu.Item name='WORKS' active={activeItem === 'Experience'} onClick={this.handleItemClick} />
-                <Menu.Item name='ABOUT' active={activeItem === 'ABOUT'} onClick={this.handleItemClick} />
-                <Menu.Item name='CONTACT' active={activeItem === 'CONTACT'} onClick={this.handleItemClick} />
-                <Menu.Menu position='right'>
-                  <Menu.Item name='JG' active={activeItem === 'JAIME GONZALEZ'} onClick={this.handleItemClick} />
-                </Menu.Menu>
-              </Menu>
-            </Container>
-            </Segment>
-          </Visibility>
-      </Responsive>
+                <Container>
+                  <Menu id='navbar' pointing secondary fixed='bottom' size='massive'>
+                    <Menu.Item name='WORKS' active={activeItem === 'WORKS'} onClick={this.handleItemClick} />
+                    <Menu.Item name='ABOUT' active={activeItem === 'ABOUT'} onClick={this.handleItemClick} />
+                    <Menu.Item name='CONTACT' active={activeItem === 'CONTACT'} onClick={this.handleItemClick} />
+                    <Menu.Menu position='right'>
+                      <Menu.Item name='JAIME GONZALEZ' active={activeItem === 'JAIME GONZALEZ'} onClick={this.handleItemClick} />
+                    </Menu.Menu>
+                  </Menu>
+                </Container>
+              </Segment>
+            </Visibility>
+          </Responsive>
+          <Responsive {...Responsive.onlyMobile}>
+            <Visibility>
+              <Segment textAlign='center' style={{ minHeight: 700, padding: '7em 0em' }} vertical>
+                {activeItem === 'JAIME GONZALEZ' ? (
+                  <div>
+                    <Hero small='Jaime Gonzalez | Full Stack Developer' />
+                    <Hero big='Hello.' para='Come check out my work!' />
+                  </div>
+                ) : activeItem === 'ABOUT' ? (
+                  <div>
+                    <Hero big='ABOUT.' />
+
+                    <RightCard
+                      para='I am a full-stack Software Developer with a focus on User Interfaces and User Experience.
+            My most recent work has been built in VueJS, Angular, NodeJS, and MongoDB, however I started my career in 
+            .NET and Sql. Javascript is definitely my favorite though.  
+            '
+                    />
+                  </div>
+                ) : activeItem === 'CONTACT' ? (
+                  <div>
+                    <Hero big='CONTACT.' small='You can reach me anywhere' />
+                    <SmallGrid github={Github} link={LinkedIn} res={UP} mail={Mail} />
+                  </div>
+                ) : activeItem === 'WORKS' ? (
+                  <Work big='WORKS.' />
+                ) : null}
+                <Container>
+                  <Menu id='navbar' fixed='top' pointing secondary size='large'>
+                    <Menu.Item name='WORKS' active={activeItem === 'Experience'} onClick={this.handleItemClick} />
+                    <Menu.Item name='ABOUT' active={activeItem === 'ABOUT'} onClick={this.handleItemClick} />
+                    <Menu.Item name='CONTACT' active={activeItem === 'CONTACT'} onClick={this.handleItemClick} />
+                    <Menu.Menu position='right'>
+                      <Menu.Item name='JG' active={activeItem === 'JAIME GONZALEZ'} onClick={this.handleItemClick} />
+                    </Menu.Menu>
+                  </Menu>
+                </Container>
+              </Segment>
+            </Visibility>
+          </Responsive>
+          <Responsive {...Responsive.onlyTablet}>
+            <Visibility>
+              <Segment textAlign='center' style={{ minHeight: 700, padding: '7em 0em' }} vertical>
+                {activeItem === 'JAIME GONZALEZ' ? (
+                  <div>
+                    <Hero small='Jaime Gonzalez | Full Stack Developer' />
+                    <Hero big='Hello.' para='Come check out my work!' />
+                  </div>
+                ) : activeItem === 'ABOUT' ? (
+                  <div>
+                    <Hero big='ABOUT.' />
+
+                    <RightCard
+                      para='I am a full-stack Software Developer with a focus on User Interfaces and User Experience.
+            My most recent work has been built in VueJS, Angular, NodeJS, and MongoDB, however I started my career in 
+            .NET and Sql. Javascript is definitely my favorite though. 
+            '
+                    />
+                  </div>
+                ) : activeItem === 'CONTACT' ? (
+                  <div>
+                    <Hero big='CONTACT.' small='You can reach me anywhere' />
+                    <SmallGrid github={Github} link={LinkedIn} res={UP} mail={Mail} />
+                  </div>
+                ) : activeItem === 'WORKS' ? (
+                  <Work big='WORKS.' />
+                ) : null}
+                <Container>
+                  <Menu id='navbar' fixed='top' pointing secondary size='large'>
+                    <Menu.Item name='WORKS' active={activeItem === 'Experience'} onClick={this.handleItemClick} />
+                    <Menu.Item name='ABOUT' active={activeItem === 'ABOUT'} onClick={this.handleItemClick} />
+                    <Menu.Item name='CONTACT' active={activeItem === 'CONTACT'} onClick={this.handleItemClick} />
+                    <Menu.Menu position='right'>
+                      <Menu.Item name='JG' active={activeItem === 'JAIME GONZALEZ'} onClick={this.handleItemClick} />
+                    </Menu.Menu>
+                  </Menu>
+                </Container>
+              </Segment>
+            </Visibility>
+          </Responsive>
+        </div>
       </div>
-      </div>
-    );
+    )
   }
 }
 
-export default navbar;
-
+export default navbar
